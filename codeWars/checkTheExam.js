f501bc0b91085ec647e69c8ee93f82d6e2a642a2
Daniel Ortiz <danielortiz08@gmail.com>
2019-12-23 18:15:33 -0500

    Added a file

---
+++ codeWars/checkTheExam.js
@@ -0,0 +1,16 @@
+function checkExam(array1, array2){
+let count=0
+  for(let i=0;i<array1.length;i++){
+    if(array1[i]===array2[i]){
+      count+=4
+    }else if(array2[i]===""){
+      count+=0
+    }else if(array1[i]!==array2[i]){
+      count-=1
+    }
+  }
+  if(count<0){
+  count=0
+  }
+  return count
+}
