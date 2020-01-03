a7e40b2447972723a9c9149016449e8832bc4a92
Daniel Ortiz <danielortiz08@gmail.com>
2020-01-03 17:06:11 -0500

    Added file

---
+++ codeWars/appleTurnover.js
@@ -0,0 +1,8 @@
+function apple(x){
+let num = parseInt(x)
+  if(num**2 > 1000){
+    return "It\'s hotter than the sun!!"
+  }else{
+    return "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
+}
