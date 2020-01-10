3bdbeb2d116c713a51463a11bc423967e4ec6b9b
Daniel Ortiz <danielortiz08@gmail.com>
2020-01-10 10:04:08 -0500

    Added file

---
+++ codeWars/ufc.js
@@ -0,0 +1,8 @@
+var quote = function(fighter) {
+  let fight = fighter.toLowerCase();
+  if(fight === 'george saint pierre'){
+    return "I am not impressed by your performance."
+  }else{
+    return "I\'d like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+}
