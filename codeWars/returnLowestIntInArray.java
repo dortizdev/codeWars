5e3f76739286eaf23f5bf8443ed6e59f3bd6ffae
Daniel Ortiz <danielortiz08@gmail.com>
2021-06-18 16:17:07 -0400

    Add java solution for smallest Int

---
+++ codeWars/returnLowestIntInArray.java
@@ -0,0 +1,11 @@
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+      int num = Integer.MAX_VALUE;
+      for(int i=0; i<args.length;i++) {
+        if (args[i]<num) {
+          num = args[i];
+        }
+      }
+      return num;
+    }
+}
